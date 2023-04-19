db9c28570e1047b7e6092dacd73ec76d8544d9b9
Piotr Wojcik <splaree@gmail.com>
2023-04-19 19:35:26 +0200

    fixed BEM names

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ const age = 28;
 console.log(
   `Siema. Nazywam się ${firstName} i mam ${age} lat. Witam CIEBIE na MOJEJ stronie.`
 );
+
+
+
+const heading = document.querySelector('.title-header__title--js');
+console.log(heading.innerHTML);
+
+heading.innerHTML = `Siema. Nazywam się ${firstName} i mam ${age} lat. Witam CIEBIE na MOJEJ stronie.`;
+console.log(heading.innerHTML);
